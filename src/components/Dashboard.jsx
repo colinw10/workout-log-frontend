690cadb60c66971df532c0280bce1ddff5833eaa
Colin W <colinweir2003@gmail.com>
2025-11-07 14:24:19 -0500

    dashboard

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,16 @@
+import { useContext } from 'react';
+import { UserContext } from '../contexts/UserContext';
+import styles from './Dashboard.module.css';
+
+const Dashboard = () => {
+  const { user } = useContext(UserContext);
+
+  return (
+    <main className={styles.container}>
+      <h1>Welcome, {user.name}</h1>
+      <p>Select "My Workouts" to see your logs or "New Workout" to add one.</p>
+    </main>
+  );
+};
+
+export default Dashboard;
