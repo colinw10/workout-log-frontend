948ff2d20092001f7b9939d8e5e89137e31ee297
Colin W <colinweir2003@gmail.com>
2025-11-12 19:19:08 -0500

    Title

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -3,7 +3,7 @@ import styles from './Landing.module.css';
 const Landing = () => {
   return (
     <main className={styles.container}>
-      <h1>Welcome to Your Workout Log</h1>
+      <h1>Welcome to My Workout Log</h1>
       <h3>Please log in or sign up to continue.</h3>
     </main>
   );
